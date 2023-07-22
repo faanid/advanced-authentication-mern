d4f714e1d4ef2a4fe9eaa64012f64cba5851d929
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-07-22 17:47:06 +0430

    Create header component with navigation

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "../../components/header/Header";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <Header />
+      <h1>Home!</h1>
     </div>
   );
 };
