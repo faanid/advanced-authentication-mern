b1e7e75e73a13683a8e79e44b04e4cd989ddc59f
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-07-29 22:54:00 +0430

    feat: ui login page

--- frontend/src/pages/auth/Login.js
+++ frontend/src/pages/auth/Login.js
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import Link from "react-router-dom";
+import { BiLogIn } from "react-icons/bi";
 import styles from "./auth.module.scss";
 import Card from "../../components/card/Card";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleInputChange = () => {};
+
+  const loginUser = () => {};
+
   return (
     <div className={`container ${styles.auth}`}>
-      <Card> </Card>
+      <Card>
+        <div className={styles.form}>
+          <div className="--flex-center">
+            <BiLogIn size={35} color="#999" />
+          </div>
+          <h2>Login</h2>
+          <div className="--flex-center">
+            <button className="--btn --btn-google">Login With Google </button>
+          </div>
+          <br />
+          <p className="--text-center --fw-bold">or</p>
+          <form onSubmit={loginUser}>
+            <input
+              type="email"
+              placeholder="Email"
+              required
+              name="email"
+              value={email}
+              onChange={handleInputChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              name="password"
+              value={password}
+              onChange={handleInputChange}
+            />
+            <button type="submit" className="--btn --btn-primary --btn-block">
+              Login
+            </button>
+          </form>
+          <Link to="/forget">Forget Password</Link>
+          <span className={styles.register}>
+            <Link to="/">Home</Link>
+            <p> &nbsp; Don't have an account? &nbsp;</p>
+            <Link to="/register">Register</Link>
+          </span>
+        </div>
+      </Card>
     </div>
   );
 };
