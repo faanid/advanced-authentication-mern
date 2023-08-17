71a834ad25f449b91ddc14eb573e0caef1159e01
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-08-17 22:59:53 +0430

    feat: Change Role and Loader components

--- frontend/src/pages/userList/UserList.js
+++ frontend/src/pages/userList/UserList.js
@@ -4,6 +4,7 @@ import PageMenu from "../../components/pageMenu/PageMenu";
 import UserStats from "../../components/userStats/UserStats";
 import Search from "../../components/search/Search";
 import { FaTrashAlt } from "react-icons/fa";
+import ChangeRole from "../../components/ChangeRole/ChangeRole";
 
 function UserList() {
   return (
@@ -22,36 +23,37 @@ function UserList() {
                 <Search />
               </span>
             </div>
-          </div>
-
-          {/* Table */}
-          <div>
-            <table>
-              <thead>
-                <tr>
-                  <th>s/n</th>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Role</th>
-                  <th>Change Role</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>fateme</td>
-                  <td>fateme@gmail.com</td>
-                  <td>Admin</td>
-                  <td>Change Role</td>
-                  <td>
-                    <span>
-                      <FaTrashAlt size={20} color="red" />
-                    </span>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            {/* Table */}
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <th>s/n</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Change Role</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>fateme</td>
+                    <td>fateme@gmail.com</td>
+                    <td>Admin</td>
+                    <td>
+                      <ChangeRole />
+                    </td>
+                    <td>
+                      <span>
+                        <FaTrashAlt size={20} color="red" />
+                      </span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
