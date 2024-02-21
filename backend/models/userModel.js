0dacb8426129ee95c77313021724c03863c0870e
faanid <73032767+faanid@users.noreply.github.com>
2024-02-21 20:17:56 +0330

    feat: Create User Model

---
+++ backend/models/userModel.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+      trim: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please enter a valid email",
+      ],
+      password: {
+        type: String,
+        required: [true, "Please add a password"],
+      },
+      photo: {
+        type: String,
+        required: [true, "Please add a photo"],
+        default:
+          "https://github.com/zinotrust/auth-app-styles/blob/master/assets/avatarr.png",
+      },
+      phone: {
+        default: "+234",
+      },
+      bio: {
+        type: String,
+        default: "bio",
+      },
+      role: {
+        type: String,
+        required: true,
+        default: "subscriber",
+        //subscriber , author , and admin (suspended)
+      },
+      isVerified: {
+        type: Boolean,
+        default: false,
+      },
+      userAgent: {
+        //the device the user is sign in
+        type: Array,
+        required: true,
+        default: [],
+      },
+    },
+  },
+  {
+    timeStamps: true,
+    minimize: false,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
