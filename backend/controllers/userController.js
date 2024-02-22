65cfe16c7ae25ee22051360f23ad59fed6733754
faanid <73032767+faanid@users.noreply.github.com>
2024-02-22 15:52:40 +0330

    feat: Register user-1

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,36 @@
-const registerUser = async (req, res) => {
-  res.send("Register user");
-};
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill in al the required fields.");
+  }
+
+  if (password.length < 6) {
+    res.status(400);
+    throw new Error("Password must be up to 6 characters.");
+  }
+
+  // Check If User Exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("Email already in use. Please login instead.");
+  }
+
+  // Create New User
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+});
 
 module.exports = {
   registerUser,
