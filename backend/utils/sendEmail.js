cfbd1990823b44db40c4947648dad7eff1fde096
faanid <73032767+faanid@users.noreply.github.com>
2024-03-01 18:04:15 +0330

    feat: Create Send Email function

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,60 @@
+const nodemailer = require("nodemailer");
+const hbs = require("nodemailer-express-handlebars");
+const path = require("path");
+
+const sendEmail = async (
+  subject,
+  send_to,
+  sent_from,
+  reply_to,
+  template,
+  name,
+  link
+) => {
+  // Create Email Transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: 587,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  const handlebarOptions = {
+    viewEngine: {
+      extName: ".handlebars",
+      partialsDir: path.resolve("./views"),
+      defaultLayout: false,
+    },
+    viewPath: path.resolve("./views"),
+    extName: ".handlebars",
+  };
+
+  // Options for sending email
+  const options = {
+    from: sent_from,
+    to: send_to,
+    replyTo: reply_to,
+    subject,
+    template,
+    context: {
+      name,
+      link,
+    },
+  };
+
+  // Send Email
+  transporter.sendMail(options, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+};
+
+module.exports = sendEmail;
