1d6d48d04627462f19c61c592eb760e7f6232ad4
faanid <73032767+faanid@users.noreply.github.com>
2024-03-03 15:42:37 +0330

    feat: create token model

---
+++ backend/models/tokenModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const tokenSchema = mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "user",
+  },
+  verificationToken: {
+    type: String,
+    default: "",
+  },
+  resetToken: {
+    type: String,
+    default: "",
+  },
+  loginToken: {
+    type: String,
+    default: "",
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+  expiresAt: {
+    type: Date,
+    required: true,
+  },
+});
+
+const Token = mongoose.model("Token", tokenSchema);
+module.exports = Token;
