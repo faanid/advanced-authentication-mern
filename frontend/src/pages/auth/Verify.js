aee7ab0a3eda9c2525c2b244a024153ba258cd09
faanid <73032767+faanid@users.noreply.github.com>
2024-04-28 16:52:47 +0330

    feat: verifyUser component logic

--- frontend/src/pages/auth/Verify.js
+++ frontend/src/pages/auth/Verify.js
@@ -1,16 +1,31 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { RESET, verifyUser } from "../../redux/features/auth/authSlice";
+import Loader from "../../components/Loader/Loader";
 
-function Verify() {
+const Verify = () => {
+  const dispatch = useDispatch();
+  const { verificationToken } = useParams();
+  const { isLoading } = useSelector((state) => state.auth);
+
+  const verifyAccount = async () => {
+    await dispatch(verifyUser(verificationToken));
+    await dispatch(RESET());
+  };
   return (
     <section>
+      {isLoading && <Loader />}
       <div className="--center-all">
         <h2>Account Verification</h2>
         <p>To verify you account, click the button below...</p>
         <br />
-        <button className="--btn --btn-primary">Verify Account</button>
+        <button onClick={verifyAccount} className="--btn --btn-primary">
+          Verify Account
+        </button>
       </div>
     </section>
   );
-}
+};
 
 export default Verify;
